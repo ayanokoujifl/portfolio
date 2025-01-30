23cd5c50f1dadcc84fcbe1b458d9786166efdd28
ayanokoujifl <guleite3@gmail.com>
2025-01-30 15:51:22 -0300

    web card projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,7 +2,9 @@
 
 import { Card } from "@/components/card"
 import { Header } from "@/components/header"
-import Atomic from "@/assets/atomic.png"
+import OR from "@/assets/projects/OR.png"
+import Events from "@/assets/projects/events.png"
+import Pokedex from "@/assets/projects/pokedex.png"
 
 export default function Projects() {
   return (
@@ -14,11 +16,28 @@ export default function Projects() {
         </h1>
         <section className="flex flex-col py-4 bg-gray-100 px-3">
           <h3>Web</h3>
-          <div className="flex flex-1 flex-wrap justify-between">
+          <p className="text-xs text-gray-600 mb-3">
+            I've done a lot of web projects, but I'll just show some of them as
+            a demonstration
+          </p>
+          <div className="flex flex-1 flex-wrap justify-center  gap-4 lg:gap-8">
             <Card
               title="Advocacia Oliveira Rosa"
-              thumbnail={Atomic}
-              redirect=""
+              thumbnail={OR}
+              redirect="https://oliveira-rosa.vercel.app"
+              description="A Website developed for Advocacia Oliveira Rosa to enterprise marketing. Developed with framework Next.js"
+            />
+            <Card
+              title="Platform for video class"
+              thumbnail={Events}
+              redirect="https://event-platform-s8sv.vercel.app"
+              description="A Website developed for streaming of videos classes. I developed that when I was studying in the IFMG-sje, on the year 2022. Developed with React.js"
+            />
+            <Card
+              title="Pokedex"
+              thumbnail={Pokedex}
+              redirect="https://pokedex-teal-kappa.vercel.app"
+              description="This is a Pokedex, developed when I was learning how to consume APIs, on the year 2021. Developed with Angular.js"
             />
           </div>
         </section>
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -4,16 +4,29 @@ type Props = {
   title: string
   thumbnail: StaticImageData
   redirect: string
+  description: string
 }
 
-export function Card({ title, thumbnail, redirect }: Props) {
+export function Card({ title, thumbnail, redirect, description }: Props) {
   return (
     <>
-      <div className="flex flex-col items-center border-2 border-slate-900 rounded w-64 h-48 justify-center relative cursor-pointer">
-        <Image src={thumbnail} alt={title} className="size-24" />
-        <h6 className="text-xs font-bold text-gray-400  absolute bottom-0 selection:bg-transparent selection:text-cyan-800 cursor-default ">
-          {title}
-        </h6>
+      <div
+        className="relative card-wrapper"
+        onClick={() => window.open(redirect, "_blank")}
+      >
+        <div className="flex flex-col items-center border-2 border-gray-700 rounded w-[28rem] aspect-video justify-center cursor-pointer relative card-front">
+          <Image
+            src={thumbnail}
+            alt={title}
+            className="h-full absolute top-0"
+          />
+          <h6 className="text-xs font-bold text-gray-300  absolute bottom-0 selection:bg-transparent selection:text-cyan-800 cursor-default shadow-text">
+            {title}
+          </h6>
+        </div>
+        <div className="absolute top-0 left-0  card-back bg-gray-100/90 flex flex-col items-center border-2 border-gray-700 rounded justify-center cursor-pointer p-6">
+          <p className="text-justify text-slate-900 text-lg ">{description}</p>
+        </div>
       </div>
     </>
   )
